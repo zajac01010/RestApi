d8db7e1ca2ea52bbf9ee454eca78fa292bc38b42
Your Name <zajac01010@gmail.com>
2019-07-31 08:33:31 +0200

    initial commit

---
+++ src/main/java/com/example/webservice/controllers/App.java
@@ -0,0 +1,14 @@
+package com.example.webservice.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+
+	}
+
+}
